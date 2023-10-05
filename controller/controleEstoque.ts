a56a42557c0be416fb997b14cd2db6f9bca5f50c
unknown <arifilhosilva235@gmail.com>
2023-10-05 15:42:28 -0300

    feat: adiciona o restante das funções necessárias

--- controller/controleEstoque.ts
+++ controller/controleEstoque.ts
@@ -1,10 +1,16 @@
+import { error } from 'console';
 import { insert, getObject, remove, list } from '../service/serviceEstoque';
 
 export const inserirEstoque = async (filePath, estoque) => {
-    try {
-        return insert(filePath, estoque);
-    } catch (error) {
-        throw error;
+    if (await buscarPorNome(filePath, estoque.nome) == null) {
+        try {
+            return insert(filePath, estoque);
+        } catch (error) {
+            throw error;
+        }
+    }
+    else {
+        throw "Esse item já existe no inventário";
     }
 };
 
@@ -31,3 +37,102 @@ export const listarTodos = async (filePath) => {
         throw error;
     }
 };
+
+export const totalValorInventario = async (filePath) => {
+    try {
+        const dados = await list(filePath);
+        var soma = 0;
+        if (dados.length == 0) {
+            return 0;
+        }
+        else {
+            dados.forEach((dado, index) => {
+                soma += dado.valor*dado.quantidade;
+            });
+            return soma;
+        }
+    } catch (error) {
+        throw error;
+    }
+}
+
+export const totalPesoInventario = async (filePath) => {
+    try {
+        const dados = await list(filePath);
+        var soma = 0;
+        if (dados.length == 0) {
+            return 0;
+        }
+        else {
+            dados.forEach((dado, index) => {
+                soma += dado.peso*dado.quantidade;
+            });
+            return soma;
+        }
+    } catch (error) {
+        throw error;
+    }
+}
+
+export const mediaValorInventario = async (filePath) => {
+    try {
+        const dados = await list(filePath);
+        var soma = 0;
+        if (dados.length == 0) {
+            return 0;
+        }
+        else {
+            dados.forEach((dado, index) => {
+                soma += dado.valor*dado.quantidade;
+            });
+            return soma/dados.length;
+        }
+    } catch (error) {
+        throw error;
+    }
+}
+
+export const mediaPesoInventario = async (filePath) => {
+    try {
+        const dados = await list(filePath);
+        var soma = 0;
+        if (dados.length == 0) {
+            return 0;
+        }
+        else {
+            dados.forEach((dado, index) => {
+                soma += dado.peso*dado.quantidade;
+            });
+            return soma/dados.length;
+        }
+    } catch (error) {
+        throw error;
+    }
+}
+
+export const quantidadeItensInventario = async (filePath) => {
+    try {
+        const dados = await list(filePath);
+        var soma = 0;
+        if (dados.length == 0) {
+            return 0;
+        }
+        else {
+            dados.forEach((dado, index) => {
+                soma += dado.quantidade;
+            });
+            return soma;
+        }
+    } catch (error) {
+        throw error;
+    }
+}
+
+export const quantidadeProdutosInventario = async (filePath) => {
+    try {
+        const dados = await list(filePath);
+        return dados.length;
+    } catch (error) {
+        throw error;
+    }
+}
