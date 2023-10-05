814ea13c24419689e47cf2286bbc4668fd2d15ec
unknown <arifilhosilva235@gmail.com>
2023-10-05 09:53:26 -0300

    feat: remove estoque pelo nome no db

--- service/serviceEstoque.ts
+++ service/serviceEstoque.ts
@@ -17,12 +17,33 @@ export const insert = async (filePath: string, data: Data[]): Promise<void> => {
   return csvWriter.writeRecords(data);
 };
 
-
 export const getObject = async (filePath: string, nome: string): Promise<Data | undefined> => {
   try {
     const dados = await list(filePath);
     const objetoEncontrado = dados.find((objeto) => objeto.nome === nome);
     return objetoEncontrado;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const remove = async (filePath: string, nome: string): Promise<void> => {
+  try {
+    const dados = await list(filePath);
+    const objetoIndex = dados.findIndex((objeto) => objeto.nome === nome);
+
+    if (objetoIndex !== -1) {
+      dados.splice(objetoIndex, 1);
+      const fileStream = fs.createWriteStream(filePath);
+      fileStream.write('nome,peso,valor,quantidade\n');
+      dados.forEach((objeto) => {
+        fileStream.write(`${objeto.nome},${objeto.peso},${objeto.valor},${objeto.quantidade}\n`);
+      });
+      fileStream.end();
+      console.log(`Objeto com o nome "${nome}" removido com sucesso.`);
+    } else {
+      console.log(`Nenhum objeto encontrado com o nome "${nome}".`);
+    }
   } catch (error) {
     throw error;
   }
